d9c11a4602611e558b121aec632c96e40f3e598c
Vivica Cong Ma <vivicacongma@gmail.com>
2020-09-27 20:35:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // Code your solution here!
 
+function printString(str){
+  console.log(str[0])
+  if(str.length > 1){
+    let sub = str.substring(1, str.length)
+    printString(sub)
+  }else{
+    "we are done"
+  }
+}
+
+
+function reverseString(str){
+  let first = 
+  let sub = myString.substring(1, str.length)
+  return sub.push()
+  
+  
+}
